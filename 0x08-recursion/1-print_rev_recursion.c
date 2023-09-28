485fcd77673ee0f8dc249d945b8136450926fee4
Patrick Murani <patrickmurani@gmail.com>
2023-09-28 12:18:39 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,13 +9,13 @@ void _print_rev_recursion(char *s)
 	if (*s == '\0')
 	{
 		_putchar('\n');
-		return (void);
+		return;
 	}
 	else
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		return (void);
+		return;
 	}
 }
 
