2e9656f919f751d21c46de67620f5eb4f3c6e443
Patrick Murani <patrickmurani@gmail.com>
2023-10-04 19:14:29 -0700

    print my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints its name
+ * @argc: parameter
+ * @argv: parameter
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	if (argc >= 1)
+	{
+		printf("%s", argv[0]);
+	}
+	return (0);
+}
