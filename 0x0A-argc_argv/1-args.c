826b2a361994b50b3e69aa5345deaa9aa6fb1c84
Patrick Murani <patrickmurani@gmail.com>
2023-10-04 19:28:33 -0700

    number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints number of command line arguements
+ * @argc: number
+ * @argv: array
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+}
