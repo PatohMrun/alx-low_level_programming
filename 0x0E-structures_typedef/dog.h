a40b831d368d6aa2c26ea0b5195810f39faa24f1
Patrick Murani <patrickmurani@gmail.com>
2023-10-10 15:22:31 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - new type struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
